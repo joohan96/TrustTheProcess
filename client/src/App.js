128bb607c4f138a1c9cdb2082ec0b4fb855a5da6
jason <jason_gan96@hotmail.com>
2019-05-09 13:37:01 -0700

    Template UI and Log in working (if already logged in)

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,64 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn react
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+
+  constructor(props) {
+      super(props);
+      this.state = {
+      };
+  }
+  
+
+  login() {
+    // HTTPS might get blocked by CORS
+     return fetch('http://127.0.0.1:3001/auth/facebook')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      console.log(responseJson);
+      // user profile from mongodb
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+  
+
+  render() {
+    return (
+      <div className="inner-container">
+        <div className="header">
+          Template
+        </div>
+        <div className="box">
+
+          <div className="input-group">
+            <label htmlFor="username">Email</label>
+            <input
+              type="text"
+              name="email"
+              className="login-input"
+              placeholder="Email"/>
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              className="login-input"
+              placeholder="Password"/>
+          </div>
+
+          <button
+            type="button" onClick={  this.login }>Log In With Facebook
+            </button>
+        </div>
+      </div>
+    );
+}
+
 }
 
 export default App;
